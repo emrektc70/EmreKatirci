cd2ec1bcaf4f840704f5958952372a17502cb3f8
emrektc70 <emrektc70@gmail.com>
2023-11-16 21:23:58 +0100

    google ana

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import AboutSection from "./components/AboutSection";
 import AchievementsSection from "./components/ArchivementSection";
 import EmailSection from "./components/EmailSection";
@@ -5,8 +6,40 @@ import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import { Projetcs } from "./components/Projetcs";
+import { useEffect } from 'react';
 
+
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
 export default function Home() {
+  useEffect(() => {
+    // Ajoutez le code de suivi Google Analytics
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-Q47Q5BYV7H';
+    script.async = true;
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function () {
+        window.dataLayer.push(arguments);
+      };
+
+      window.gtag('js', new Date());
+      window.gtag('config', 'G-Q47Q5BYV7H');
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      // Nettoyage si nécessaire
+    };
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
       <Navbar />
