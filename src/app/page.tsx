e21c6eb8165a20c4dc72429032fc24269e312251
Emre Katirci <emrektc70@gmail.com>
2026-02-18 13:53:58 +0100

    fix: change offside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,8 +40,6 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
-        {/*         <AchievementsSection />
-         */}
         <AboutSection />
         <Projetcs />
         <EmailSection />
